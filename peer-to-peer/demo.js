3cf847c0b9d2942feb03e22ae139dd5b1e3595ce
Znapi <zachary.napier.44@gmail.com>
2015-05-20 15:19:13 -0700

    added include function

--- peer-to-peer/demo.js
+++ peer-to-peer/demo.js
@@ -1,10 +1,18 @@
 (function(ext) {
+
+    /**************************
+    * Required extension code *
+    ***************************/
+
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+    ext._shutdown = function() {
+      //TODO tell helper app to close
+    };
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
+        //TODO ping helper app
         return {status: 2, msg: 'Ready'};
     };
 
@@ -22,4 +30,36 @@
 
     // Register the extension
     ScratchExtensions.register('My first extension', descriptor, ext);
+
+
+    /************************
+     * Comm with helper app *
+     ************************/
+
+     /**
+      Epic code found in this SO answer: http://stackoverflow.com/a/20518446/3390450
+      Uses an ajax to load a file on the same domain synchonously and without jQuery,
+      with purely JavaScript, no html <script> tags. Helpful for Scratch JavaScript
+      extensions, where there is no html file to put tags in.
+     */
+     function includeFile(url) {
+       var ajax = new XMLHttpRequest();
+       ajax.open('GET', url, false); // <-- the 'false' makes it synchronous
+       ajax.onreadystatechange = function() {
+         var script = ajax.response || ajax.responseText;
+         if(ajax.readyState === 4) {
+           switch(ajax.status) {
+             case 200:
+             eval.apply(window, [script]);
+             console.log("script loaded: ", url);
+             break;
+
+             default:
+             console.log("ERROR: script not loaded: ", url);
+           }
+         }
+       };
+       ajax.send(null);
+     }
+
 })({});
