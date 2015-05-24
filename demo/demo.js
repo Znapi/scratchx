470028384dba5a7b9f4d728ab90f015c7e8cf321
Znapi <zachary.napier.44@gmail.com>
2015-05-23 19:48:11 -0700

    fixed indentation

--- demo/demo.js
+++ demo/demo.js
@@ -33,50 +33,57 @@
 
   ScratchExtensions.register('Demo extension', descriptor, ext);
 
-
+// <<
 
   /******************************\
   * Program begins here on load  *
   \******************************/
 
 
-  /**
-  Credit to here: http://stackoverflow.com/a/20518446/3390450,
-  Small change to be asynchronomous.
-  It includes a .js file by url, and to prevent warnings, it is done
-  asynchronoumously and calls a callback function when it's done, and whose only
-  parameter should be a boolean that says if the file could be loaded or not.
-  */
-  function includeFile(url, callback) {
-    var ajax = new XMLHttpRequest();
-    ajax.open('GET', url, true); // <-- a 'false' makes it synchronous
-    ajax.onreadystatechange = function() {
-      var script = ajax.response || ajax.responseText;
-      if(ajax.readyState === 4) {
-        switch(ajax.status) {
-          case 200:
-          eval.apply(window, [script]);
-          console.log("script loaded: ", url);
-          callback(true);
-          break;
+/**
+Credit to here: http://stackoverflow.com/a/20518446/3390450,
+Small change to be asynchronomous.
+It includes a .js file by url, and to prevent warnings, it is done
+asynchronoumously and calls a callback function when it's done, and whose only
+parameter should be a boolean that says if the file could be loaded or not.
+*/
+function includeFile(url, callback) {
+  var ajax = new XMLHttpRequest();
+  ajax.open('GET', url, true); // <-- a 'false' makes it synchronous
+  ajax.onreadystatechange = function() {
+    var script = ajax.response || ajax.responseText;
+    if(ajax.readyState === 4) {
+      switch(ajax.status) {
+        case 200:
+        eval.apply(window, [script]);
+        console.log("script loaded: ", url);
+        callback(true);
+        break;
 
-          default:
-          console.log("ERROR: script not loaded: ", url);
-          callback(false);
-        }
+        default:
+        console.log("ERROR: script not loaded: ", url);
+        callback(false);
       }
-    };
-    ajax.send(null);
-  }
-
-  // Start by retrieveing socket.io, which is necessary to connect to helper app
-  includeFile("http://znapi.github.io/scratchx/demo/socket.io.min.js", function(gotSocketIO) {
-    if(!gotSocketIO) {
-      ext._getStatus=function(){return{status:0, msg:'Could not retrieve socket.io'}};
-      // This extension does no more work from here on because the socket.io was not loaded
     }
-    else {
-      // Continue to next step of initialization
+  };
+  ajax.send(null);
+}
+var resourcesURL = "http://znapi.github.io/scratchx/demo/";
+
+// Initialization: Step 1
+// Start by retrieveing socket.io, which is necessary to connect to helper app
+includeFile(resourcesURL + "socket.io.min.js", getBlocks);
+
+// Initialization: Step 2
+!function getBlocks(gotSocketIO) {
+  if(!gotSocketIO) {
+    ext._getStatus=function(){return{status:0, msg:'Could not retrieve socket.io'}};
+    // This extension does no more work from here on because the socket.io was not loaded
+  }
+  else {
+    // Continue to next step of initialization: set block functions
+    //includeFile(resourcesURL + "blocks.js", )
+    // Continue to last step of initialization: set up connection to helper app
 // <<
 
 var connected = false;
@@ -105,7 +112,7 @@ socket.on('disconnect', function() {
 });
 
 // <<
-    }
-  });
-
+  }
+}
+// <<
 })({});
