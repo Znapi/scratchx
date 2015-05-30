2985bb2f169457948483dd5fa37beadfc21a558f
Znapi <zachary.napier.44@gmail.com>
2015-05-30 11:57:34 -0700

    updated

--- demo/main-http.js
+++ demo/main-http.js
@@ -76,7 +76,7 @@ var url = "http://localhost:25565/"
 function reserveHelperApp() {
   console.log("Reserving helper app");
   var ajax = new XMLHttpRequest();
-  ajax.open(null, url, true);
+  ajax.open('', url, true);
   ajax.onreadystatechange = function() {
     if(ajax.readyState === 4) {
       switch(ajax.status) {
