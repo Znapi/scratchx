4fcb225eff842b8381eecaea7acff571f8af2db2
Znapi <zachary.napier.44@gmail.com>
2015-05-30 12:33:27 -0700

    updated main.js

--- extension-status-getter/main.js
+++ extension-status-getter/main.js
@@ -9,13 +9,13 @@ ext.get_ext_status_message = function(extName) {
 }
 
 ext._shutdown=function(){};
-ext._getStatus=function(){return{status:2,msg:'Readye'}};
+ext._getStatus=function(){return{status:2,msg:'Ready'}};
 
 var descriptor = {
   blocks: [
     // Block type, block name, function name
-    ['r', 'status of extension %s', 'get_ext_status'],
-    ['r', 'staus code of extension %s', 'get_ext_status_message']
+    ['r', 'status code of extension %s', 'get_ext_status'],
+    ['r', 'staus message of extension %s', 'get_ext_status_message']
   ],
   //menus: {},
   //url: 'http://znapi.github.io/scratchx/demo/about.html'
