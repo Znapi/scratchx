fccde2cd51082995289769ee3b957fe1255ce19a
Znapi <zachary.napier.44@gmail.com>
2015-05-30 12:21:16 -0700

    made new extension

---
+++ extension-status/main.js
@@ -0,0 +1,24 @@
+(function(ext) {
+
+ext.get_ext_status = function(extName) {
+  return ScratchExtensions.getStatus(extName).status;
+}
+
+ext.get_ext_status_message = function(extName) {
+  return ScratchExtensions.getStatus(extName).msg;
+}
+
+ext._shutdown=function(){};
+ext._getStatus=function(){return{status:2,msg:'Ready'}};
+
+var descriptor = {
+  blocks: [
+    // Block type, block name, function name
+    ['r', 'status of extension %s', 'get_ext_status'],
+    ['r', 'staus code of extension %s', 'get_ext_status_message']
+  ],
+  //menus: {},
+  //url: 'http://znapi.github.io/scratchx/demo/about.html'
+};
+
+ScratchExtensions.register('Extension status getter', descriptor, ext);
