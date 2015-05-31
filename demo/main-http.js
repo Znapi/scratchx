3628d6246fe00ef8f14988cc6b2a8d51968bb28d
Znapi <zachary.napier.44@gmail.com>
2015-05-31 14:16:17 -0700

    updated

--- demo/main-http.js
+++ demo/main-http.js
@@ -145,7 +145,7 @@ function connectToHA() {
       switch(ajax.status) {
         case 200:
         console.log("Connection successful");
-        console.log((new Int8Array(ajax.response)).toString());
+        console.log((new Int8Array(ajax.response)).toLocaleString());
         break;
 
         case 409:
