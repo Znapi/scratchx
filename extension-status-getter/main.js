0e7a0d4d0a37c9754d0774b01df27028f5012341
Znapi <zachary.napier.44@gmail.com>
2015-06-01 17:47:36 -0700

    udpated

--- extension-status-getter/main.js
+++ extension-status-getter/main.js
@@ -13,7 +13,7 @@ ext._getStatus=function(){return{status:2,msg:'Ready'}};
 
 var descriptor = {
   blocks: [
-    ['r', 'status code of extension %s', 'get_ext_status'],
+    ['r', 'status code of extension %s', 'get_ext_status', 'Extension Status Getter'],
     ['r', 'staus message of extension %s', 'get_ext_status_message']
   ],
   url: 'https://github.com/Znapi/scratchx/wiki/Extension-Status-Getter'
