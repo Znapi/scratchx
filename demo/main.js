130c1e6b1ba7b4ddcba4225b3745404c81f7cded
Znapi <zachary.napier.44@gmail.com>
2015-06-01 19:20:37 -0700

    renamed files

--- demo/main.js
+++ demo/main.js
@@ -1,5 +1,4 @@
 (function(ext) {
-var resourcesURL = "http://znapi.github.io/scratchx/demo/"; // Where other neccessary js files can be found
 
 /************************************\
 * Scratch Extension boilerplate code *
@@ -70,144 +69,140 @@ The purpose of the code executed in this file is to retrieve all neccessary
 resources, and, if successful, retrieve and run the initialization file.
 */
 
-/**
-Credit to here: http://stackoverflow.com/a/20518446/3390450,
-Small change to be asynchronous.
-It includes a .js file by url, and to prevent warnings, it is done
-asynchronously and calls a callback function when it's done, and whose only
-parameter should be a boolean that says if the file could be loaded or not.
-*/
-function includeFile(url, callback) {
+var status = {status: 1, msg: "Trying to connect to helper app"};
+ext._getStatus=function(){return status}
+
+var variables = [];
+var packets; // Object with keys for packet names, and are used to access id number associated with packet
+
+var needNewPeerFinderService = true;
+var peerFinderService = null;
+ext.poll_get_peer_finder_service = function() {
+  return !(needNewPeerFinderService = false);
+}
+ext.set_peer_finder_service = function(newIP) {
+  peerFinderService = newIP;
+
+}
+var becamePeerFinderService = false;
+ext.became_peer_finder_service = function() {
+  return !(becamePeerFinderService = false);
+}
+var myIP = null;
+ext.get_ip_address = function() {
+  return myIP;
+}
+
+ext.open_gui = function() {
+  guiGo(null);
+}
+ext.create_variable = function() {
+  guiGo("create_var");
+}
+ext.delete_variable = function() {
+  guiGo("delete_var");
+}
+
+ext.queue_packet = function(packetName) {
+
+}
+ext.flush_outbound = function() {
+
+}
+ext.send_packet = function(packetName) {
+
+}
+ext.read_inbound = function(callback) {
+  callback();
+}
+var readWhenRecieved = false;
+ext.set_recieve_action = function(action) {
+  if(action === "do") readWhenRecieved = true;
+  else                readWhenRecieved = false;
+
+}
+
+function guiGo(location) {
+  if(location===null) {
+    connectToHA();
+  }
+  else if(location==="create_var") {
+    pingHA();
+  }
+  else if(location==="delete_var") {
+    disconnectFromHA();
+  }
+}
+
+function reregisterExtension() {
+  ScratchExtensions.unregister('Demo extension');
+  ScratchExtensions.register('Demo extension', descriptor, ext);
+}
+
+ext._shutdown = function() {
+
+}
+
+var url = "http://localhost:25565"
+var id = null;
+function connectToHA() {
   var ajax = new XMLHttpRequest();
-  ajax.open('GET', url, true); // <-- a 'false' makes it synchronous
+  ajax.open('POST', url, true);
+  ajax.responseType = "arraybuffer";
   ajax.onreadystatechange = function() {
-    var script = ajax.response || ajax.responseText;
     if(ajax.readyState === 4) {
       switch(ajax.status) {
         case 200:
-        eval.apply(window, [script]);
-        console.log("script loaded: ", url);
-        callback(true);
+        console.log("Connection successful");
+        var tmp = new Int8Array(ajax.response);
+        id = "/" + new String(tmp[0]) + new String(tmp[1]) + new String(tmp[2]);
+        console.log(url + id);
+        status = {status: 1, msg: "Ready"};
         break;
 
         default:
-        console.log("ERROR: script not loaded: ", url);
-        callback(false);
+        console.log("Connection failed")
       }
     }
   };
-  ajax.send(null);
+  ajax.send();
 }
 
-// Initialization: Step 1
-// Get socket.io
-includeFile(resourcesURL+"socket.io-client.min.js", // Callback
+function pingHA() {
+  var ajax = new XMLHttpRequest();
+  ajax.open('GET', url + id, true);
+  ajax.onreadystatechange = function() {
+    if(ajax.readyState === 4) {
+      switch(ajax.status) {
+        case 200:
+        console.log("Ping successful");
+        break;
 
-// Initialization: Step 2
-function(gotSocketIO) {
-  if(!gotSocketIO) {
-    ext._getStatus=function(){return{status:0, msg:'Could not retrieve socket.io'}};
-    // This extension does no more work from here on because the socket.io is neccessary for the extension
-  }
-  else {
-    // Finish initialization
-    var status = {status: 1, msg: "Trying to connect to helper app"};
-    ext._getStatus=function(){return status;}
-    // Here it begins asking the server for connection
-    var socket = io("http://localhost:25565");
-    socket.on('connect', function() {
-      console.log("Connected!");
-      status = {status: 2, msg: "Ready"};
-    });
-    socket.on('ready', function() {
-      socket.emit('get_ip');
-      socket.emit('get_packet_defs');
-      console.log("server ready...requested data");
-    });
-    socket.on('disconnect', function() {
-      console.log("Disconnected!");
-      status = {status: 1, msg: "Disconnected by helper app! Restart the helper app's server"};
-    });
-
-    var variables = [];
-
-    var packets; // Object with keys for packet names, and are used to access id number associated with packet
-    socket.on('new_packet_defs', function(newPacketNames) { // An array of packet names, with index corresponding to id
-      console.log("recieved new packet defs");
-      packets = new Object();
-      descriptor.menus.packets = new Array(0);
-      for(var index in newPacketNames) {
-        packets[newPacketNames[index]] = index;
-        descriptor.menus.packets.push(newPacketNames[index]);
+        default:
+        console.log("Ping failed")
       }
-      reregisterExtension();
-    });
+    }
+  };
+  ajax.send();
+}
 
-    ext.open_gui = function() {
-      guiGo(location);
-      socket.emit('get_packet_defs');
-    }
-    ext.create_variable = function() {
-      guiGo("create_var");
-    }
-    ext.delete_variable = function() {
-      guiGo("delete_var");
-    }
-    ext.queue_packet = function(packetName) {
-      socket.emit('queue_packet', packets[packetName]);
-    }
-    ext.flush_outbound = function() {
-      socket.emit('flush_outbound');
-    }
-    ext.send_packet = function(packetName) {
-      socket.emit('send_packet', packets[packetName]);
-    }
-    ext.read_inbound = function(callback) {
-      socket.emit('read_inbound')
-      callback();
-    }
-    var readWhenRecieved = false;
-    ext.set_recieve_action = function(action) {
-      if(action === "do") readWhenRecieved = true;
-      else readWhenRecieved = false;
-      socket.emit('set_recieve_action', readWhenRecieved);
-    }
-    var needNewPeerFinderService = true;
-    var peerFinderService = null;
-    ext.poll_get_peer_finder_service = function() {
-      return !(needNewPeerFinderService = false);
-    }
-    socket.on('get_new_peer_finder_service', function() {
-      needNewPeerFinderService = true;
-    });
-    ext.set_peer_finder_service = function(newIP) {
-      peerFinderService = newIP;
-      socket.emit('new_peer_finder_service', peerFinderService);
-    }
-    var becamePeerFinderService = false;
-    ext.became_peer_finder_service = function() {
-      return !(becamePeerFinderService = false);
-    }
-    socket.on('became_peer_finder_service', function() {
-      becamePeerFinderService = true;
-    });
-    var myIP = null;
-    ext.get_ip_address = function() {
-      return myIP;
-    }
-    socket.on('serving_ip', function(ip) {
-      myIP = ip;
-    });
+function disconnectFromHA() {
+  var ajax = new XMLHttpRequest();
+  ajax.open('DELETE', url + id, true);
+  //ajax.setRequestHeader("Cache-Control", "no-cache");
+  ajax.onreadystatechange = function() {
+    if(ajax.readyState === 4) {
+      switch(ajax.status) {
+        case 200:
+        console.log("Disconnection successful");
+        break;
 
-    function guiGo(location) {
-      socket.emit('gui', location);
+        default:
+        console.log("Disconnection failed")
+      }
     }
+  };
+  ajax.send();
+}
 
-    function reregisterExtension() {
-      ScratchExtensions.unregister('Demo extension');
-      ScratchExtensions.register('Demo extension', descriptor, ext);
-    }
-  }
-
-});
 })({});
