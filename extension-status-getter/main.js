e50089f62d4e72a99dc4109c269c53c86bfe0531
Znapi <zachary.napier.44@gmail.com>
2015-06-06 16:51:36 -0700

    small change

--- extension-status-getter/main.js
+++ extension-status-getter/main.js
@@ -12,14 +12,15 @@ ext._shutdown=function(){};
 ext._getStatus=function(){return{status:2,msg:'Ready'}};
 
 var extName = 'Extension Status Getter';
-var descriptor = {
-  blocks: [
-    ['r', 'status code of extension %s', 'get_ext_status', extName],
-    ['r', 'staus message of extension %s', 'get_ext_status_message', extName]
-  ],
-  url: 'https://github.com/Znapi/scratchx/wiki/Extension-Status-Getter'
-};
 
-ScratchExtensions.register('Extension Status Getter', descriptor, ext);
+ScratchExtensions.register(extName,
+  {
+    blocks: [
+      ['r', 'status code of extension %s', 'get_ext_status', extName],
+      ['r', 'status message of extension %s', 'get_ext_status_message', extName]
+    ],
+    url: 'https://github.com/Znapi/scratchx/wiki/Extension-Status-Getter'
+  },
+  ext);
 
 })({});
