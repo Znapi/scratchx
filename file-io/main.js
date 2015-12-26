7995cad716c0da5d74e4d86e46e85526cc476b69
Znapi <znapi@MacBook-Air.local>
2015-12-26 14:41:54 -0800

    fix help url

--- file-io/main.js
+++ file-io/main.js
@@ -24,7 +24,7 @@ ScratchExtensions.register('Simple File I/O',
         ['w', 'write %s to file %s', 'write', 'hello world', 'text.txt'],
         ['w', 'create file %s', 'make', 'text.txt']
     ],
-    url: 'https://github.com/Znapi/scratchx/wiki/Simple-File-IO'
+    url: 'https://github.com/Znapi/scratchx/wiki/Simple-File-I-O'
   },
   ext);
 
